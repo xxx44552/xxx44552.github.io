a945922c3af2f73351b342e2f15faad62d69374f
xxx44552 <xxx44552@gmail.com>
2017-12-16 09:41:35 +0200

    redirect

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.location.href = 'http://webinme.ru';
+
 window.onload = function toggleMobileMenu() {
   var el = document.getElementById('mobile-menu');
   var elem = document.getElementById('mob-nav');
@@ -14,29 +16,6 @@ function closeMenu() {
 		elem.classList.add('hiddenMenu');
 		pos.classList.remove('position');
 };
-
-/*function modalImg() {
-	let elem = document.querySelectorAll('.work-item');
-	for(let i = 0; i < elem.length; i++) {
-			elem[i].childNodes[1].onclick = function() {
-				elem[i].classList.remove('work-item');
-				elem[i].classList.add('img-modal');
-				elem[i].lastChild.classList.remove('close-modal-img-hidden');
-				elem[i].lastChild.classList.add('close-modal-img');
-		};
-		var close = document.createElement('div');
-		close.setAttribute('class', 'close-modal-img-hidden');
-		close.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
-		close.display = 'none';
-		elem[i].appendChild(close);
-		close.onclick = function() {
-			elem[i].removeAttribute('class');
-			elem[i].classList.add('work-item');
-			elem[i].lastChild.classList.remove('close-modal-img');
-			elem[i].lastChild.classList.add('close-modal-img-hidden');
-		}
-	};
-};*/
 
 function modalImg() {
 	if(window.screen.width > 760) {
